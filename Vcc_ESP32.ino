5fa48bd4415c2b48914a67f49df730cabb560b7b
jabc <jabcbaj@gmail.com>
2023-09-21 15:57:32 +0200

    version2

--- Vcc_ESP32.ino
+++ Vcc_ESP32.ino
@@ -1,8 +1,9 @@
 
-float referenciaConocida = 3.350; // Voltaje de referencia conocido en V
-byte PIN = PIN;
+float VCC = 3351.0; // Voltaje de referencia conocido en V+ esp32
+byte PIN = 25;
 
 void setup() {
+  pinMode(25, INPUT_PULLUP);
   Serial.begin(115200);
   analogReadResolution(12); // Configura la resolución del ADC a 12 bits (0-4095)
 }
@@ -10,20 +11,18 @@ void setup() {
 void loop() {
   int valorADC = analogRead(PIN);
 
-  // Calibración: Calcula el factor de escala
-  float factorDeEscala = referenciaConocida / valorADC;
-
-  // Aplica el factor de escala para obtener el voltaje calibrado
-  float voltajeCalibrado = valorADC * factorDeEscala;
+  // Calibración
+  float mapa = map(valorADC, 0, 4095, 0, VCC)/1000.0;
 
   Serial.print("Valor ADC en el pin ");
   Serial.print(PIN);
   Serial.print(": ");
   Serial.print(valorADC);
-  Serial.print(", factorDeEscala: ("); Serial.print(factorDeEscala, 4); Serial.print(")");
-  Serial.print(", Voltaje Calibrado: ");
-  Serial.print(voltajeCalibrado, 4); // Muestra el voltaje con 4 decimales
+
+  Serial.print(", Voltaje medido >> ");
+  Serial.print(mapa,2); // Muestra el voltaje con 4 decimales
   Serial.println(" V");
+
 
   delay(1000);
 }
